92fb9afee541172c638a84c014fa054d8ebb1bbc
Lillian Shieh <lillian.shieh95@gmail.com>
2018-06-11 23:48:43 -0400

    completed Q2

--- Q2.js
+++ Q2.js
@@ -1,3 +1,27 @@
 function decodeString(s) {
-	
+	if(!s.includes('[')) {
+		return s
+	}
+	let numStart = 0
+	let startBracket = s.indexOf('[')
+	// just in case if number has more than one digit
+	for(let n=startBracket-1;n>=0;n--) {
+		if(isNaN(s[n])) {
+			numStart = n+1
+			break
+		}
+	}
+	let endBracket = s.lastIndexOf(']')
+	// keep track of brackets
+	for(let i=startBracket+1;i<s.length;i++) {
+		if(s[i] === ']') {
+			endBracket = i
+			break
+		} else if(s[i] === '[') {
+			break
+		}
+	}
+	return decodeString(s.slice(0,startBracket - (startBracket - numStart)) + s.slice(startBracket+1,endBracket).repeat(parseInt(s.slice(numStart,startBracket+1))) + s.slice(endBracket+1))
 }
+
+console.log(decodeString('2[b3[a]]'))
--- Q3.js
+++ Q3.js
@@ -1,3 +1,9 @@
-function changePossibilities(amount,denominations) {
-	
+function changePossibilities(amount,denoms) {
+	let counter = 0
+	let sum = 0
+	for(let i=0;i<denoms.length;i++) {
+		while(sum < amount) {
+			sum += denoms[i]
+		}
+	}
 }
