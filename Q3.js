1ba5e8129966bdba0715072956ef56ce3f289746
Lillian Shieh <lillian.shieh95@gmail.com>
2018-06-12 13:31:29 -0400

    completed Q3

--- Q3.js
+++ Q3.js
@@ -1,9 +1,31 @@
 function changePossibilities(amount,denoms) {
-	let counter = 0
-	let sum = 0
-	for(let i=0;i<denoms.length;i++) {
-		while(sum < amount) {
-			sum += denoms[i]
+	// initialize table
+	let table = []
+	for(let d=0;d<=denoms.length;d++) {
+		table.push([])
+		for(let a=0;a<amount+1;a++) {
+			table[d].push(0)
 		}
 	}
+	// initialize first row and column for inward boxes
+	for(let i=0;i<=amount;i++) {
+		table[0][i] = 0
+	}
+	for(let i=0;i<=denoms.length;i++) {
+		table[i][0] = 1
+	}
+	// fill in table
+	for(let i=1;i<=denoms.length;i++) {
+		for(let j=1;j<=amount;j++) {
+			if(denoms[i-1] <= j) {
+				table[i][j] = table[i-1][j] + table[i][j-denoms[i-1]]
+			} else {
+				table[i][j] = table[i-1][j]
+			}
+		}
+	}
+	console.log(table)
+	return table[denoms.length][amount]
 }
+
+console.log(changePossibilities(4,[1,2,3]))
